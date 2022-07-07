2aa05a7e52333c9f0149266c9c6a381b1961d094
Yuto Mabe <yuto96315mabe@gmail.com>
2022-07-08 02:39:43 +0900

    Todoリストをhtmlに描画するところまで完成

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 // app.js
 
 import express from "express";
-import { todoRouter } from "./routes/"
+// import { todoRouter } from "./routes/"
 
 const app = express();
 const port = 3001;
 
-// ルート(http://localhost/)にアクセスしてきた時の処理
-// 第一引数のreqはなんの為？
-app.get("/", (req, res) => {
-    res.send("Success!")
+// webフォルダの中身を公開する
+app.use(express.static('web'));
+
+// http://localhost:3000/api/v1/list にアクセスしてきたときに
+// TODOリストを返す
+app.get('/api/v1/list', (req, res) => {
+    // クライアントに送るJSONデータ
+    const todoList = [
+        { title: 'JavaScriptを勉強する', done: true },
+        { title: 'Node.jsを勉強する', done: false },
+        { title: 'Web APIを作る', done: false }
+    ];
+
+    // JSONを送信する
+    res.json(todoList);
 });
 
 // todo用
-app.use("/todo", (req, res) => {
-    res.json({
-        url:"/todo",
-        messsage: "Success for accessing todo app.",
-    })
-})
+// app.use("/todo", (req, res) => {
+//     res.json({
+//         url:"/todo",
+//         messsage: "Success for accessing todo app.",
+//     })
+// })
 
 // 指定されたポート番号でサーバーを立てる処理
+// これがないとエラーが出た
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
